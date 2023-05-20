2177d528f246f397c74a2f8d28f7294a52abeebb
Jonathan Mohabir <jonamohabir416@gmail.com>
2023-05-20 19:53:51 -0400

    commits  - boilerplate code removed, established react router in front end, move on further

---
+++ server/server.js
@@ -0,0 +1,17 @@
+/*
+Backend Entry point for Jonathans Portfolio Site 
+
+Express/Node backend, Mongoose ODM
+*/
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Express Backend Home');
+});
+
+app.listen(PORT, () => {
+  console.log(`Backend Server running, press ctrl + c to end`);
+});
